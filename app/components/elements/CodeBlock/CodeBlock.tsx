7aedd4a38588e8d96a496a3d9ef371d3ec122d48
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2024-02-09 15:26:04 +0900

    コードブロックはみ出る問題の修正

--- app/components/elements/CodeBlock/CodeBlock.tsx
+++ app/components/elements/CodeBlock/CodeBlock.tsx
@@ -7,7 +7,7 @@ interface Props {
 const CodeBlock = ({ codeString }: Props) => {
   return (
     <div className="flex items-center justify-between text-gray-800 border border-gray-800 bg-white max-w-sm font-mono text-sm py-3 px-4 rounded-md">
-      <div className="flex gap-1">
+      <div className="flex gap-1 overflow-x-auto">
         <span>{codeString}</span>
       </div>
       <span className="flex text-gray-800 cursor-pointer w-5 h-5 hover:text-gray-400 duration-200">
