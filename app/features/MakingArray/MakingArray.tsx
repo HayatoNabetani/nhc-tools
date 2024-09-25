9f7c21a341926f64708686c741417296adeced56
HayatoNabetani <nabe_baseball1111@yahoo.co.jp>
2024-09-25 17:11:19 +0900

    spreadsheet

--- app/features/MakingArray/MakingArray.tsx
+++ app/features/MakingArray/MakingArray.tsx
@@ -7,6 +7,7 @@ const MakingArray = () => {
   const [arrayInput, setArrayInput] = useState<string>("");
   const [arrayResult, setArrayResult] = useState<string>("");
   const [tupleResult, setTupleResult] = useState<string>("");
+  const [spreadsheetResult, setSpreadsheetResult] = useState<string>("");
 
   const handleArrayTextAreaChange = (inputString: string) => {
     setArrayInput(inputString);
@@ -17,10 +18,18 @@ const MakingArray = () => {
     const regex = /[,;\t \n]+/;
     const arrayFromSplit = inputString.split(regex);
     setArrayResult(createArrayString(arrayFromSplit));
-    // setArrayResult(JSON.stringify(arrayFromSplit, null, 4));
+
     // SQLのIN演算子用のタプル文字列を作成
-    const tupleString = `(${arrayFromSplit.map(item => `'${item.trim()}'`).join(', ')})`;
+    const tupleString = `(${arrayFromSplit
+      .map((item) => `'${item.trim()}'`)
+      .join(", ")})`;
     setTupleResult(tupleString);
+
+    // スプレッドシートに列で貼り付けれるような形にする
+    const spreadsheetString = arrayFromSplit
+      .map((item) => item.trim())
+      .join("\n");
+    setSpreadsheetResult(spreadsheetString);
   };
 
   return (
@@ -64,10 +73,27 @@ const MakingArray = () => {
             </p>
             {/* Linter */}
             <TextArea
-              id="arrayResultTextArea"
-              textAreaLabel="arrayResultTextArea"
+              id="tupleResultTextArea"
+              textAreaLabel="tupleResultTextArea"
               placeholder="ここにタプル出力"
               value={tupleResult}
+              disabled={true}
+              onChange={handleArrayTextAreaChange}
+            />
+          </div>
+        </div>
+        <div className="flex-1 pl-1">
+          <div>
+            <p>
+              スプレッドシートに列で貼り付けれるような形にする(コピー用){" "}
+              <ButtonOnlyIcon type="copy" value={spreadsheetResult} />
+            </p>
+            {/* Linter */}
+            <TextArea
+              id="sheetResultTextArea"
+              textAreaLabel="sheetResultTextArea"
+              placeholder="ここに出力"
+              value={spreadsheetResult}
               disabled={true}
               onChange={handleArrayTextAreaChange}
             />
